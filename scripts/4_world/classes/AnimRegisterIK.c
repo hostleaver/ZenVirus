81bdb7eb398dd725cb5790e377ea9546643c9cb5
hostleaver <46068508+hostleaver@users.noreply.github.com>
2025-03-24 18:22:01 +0200

    Update and rename JMAnimRegister.c to AnimRegisterIK.c
    Animation for mod items in hands fix

--- scripts/4_world/classes/AnimRegisterIK.c
+++ scripts/4_world/classes/AnimRegisterIK.c
@@ -1,22 +1,21 @@
-modded class JMAnimRegister
+modded class ModItemRegisterCallbacks
 {
-	override void OnRegisterOneHanded(DayZPlayerType pType, DayzPlayerItemBehaviorCfg pBehavior)
+	override void RegisterOneHanded(DayZPlayerType pType, DayzPlayerItemBehaviorCfg pBehavior)
 	{
-		super.OnRegisterOneHanded(pType, pBehavior);
-
+		super.RegisterOneHanded(pType, pBehavior);
+		
 		pType.AddItemInHandsProfileIK("Zen_Virus_Cure_Injector", "dz/anims/workspaces/player/player_main/props/player_main_1h_morphine.asi", pBehavior, "dz/anims/anm/player/ik/gear/morphine.anm");
 		pType.AddItemInHandsProfileIK("Zen_Virus_Cure_Injector_Empty", "dz/anims/workspaces/player/player_main/props/player_main_1h_morphine.asi", pBehavior, "dz/anims/anm/player/ik/gear/morphine.anm");
 		pType.AddItemInHandsProfileIK("Zen_Virus_Injector", "dz/anims/workspaces/player/player_main/props/player_main_1h_morphine.asi", pBehavior, "dz/anims/anm/player/ik/gear/morphine.anm");
 		pType.AddItemInHandsProfileIK("Zen_Virus_Injector_Empty", "dz/anims/workspaces/player/player_main/props/player_main_1h_morphine.asi", pBehavior, "dz/anims/anm/player/ik/gear/morphine.anm");
-		pType.AddItemInHandsProfileIK("Zen_Virus_PathogenVial", "dz/anims/workspaces/player/player_main/props/player_main_1h_pills.asi", pBehavior, "dz/anims/anm/player/ik/gear/VitaminBottle.anm");
-		pType.AddItemInHandsProfileIK("Zen_Virus_AntiBodiesVial", "dz/anims/workspaces/player/player_main/props/player_main_1h_pills.asi", pBehavior, "dz/anims/anm/player/ik/gear/VitaminBottle.anm");
+		pType.AddItemInHandsProfileIK("Zen_Virus_PathogenVial", "dz/anims/workspaces/player/player_main/player_main_1h.asi", pBehavior, "dz/anims/anm/player/ik/gear/InjectionVial.anm");
+		pType.AddItemInHandsProfileIK("Zen_Virus_AntiBodiesVial", "dz/anims/workspaces/player/player_main/player_main_1h.asi", pBehavior, "dz/anims/anm/player/ik/gear/InjectionVial.anm");
 		pType.AddItemInHandsProfileIK("Zen_Virus_Cure_Petridish", "dz/anims/workspaces/player/player_main/player_main_1h.asi", pBehavior, "dz/anims/anm/player/ik/gear/powdered_milk.anm");
-
 	}
 
-	override void OnRegisterTwoHanded(DayZPlayerType pType, DayzPlayerItemBehaviorCfg pBehavior)
+	override void RegisterTwoHanded(DayZPlayerType pType, DayzPlayerItemBehaviorCfg pBehavior)
 	{
-		super.OnRegisterTwoHanded(pType, pBehavior);
+		super.RegisterTwoHanded(pType, pBehavior);
 
 		pType.AddItemInHandsProfileIK("Zen_Virus_Cure_Microscope", "dz/anims/workspaces/player/player_main/player_main_2h.asi", pBehavior, "dz/anims/anm/player/ik/two_handed/BatteryCar.anm");
 	}
